dae012ddff47edb3c41ef91ec27254349bac6496
Ali Nabavi <arz1379n@gmail.com>
2024-07-31 12:19:44 +0330

    Implement view models

--- app/src/main/java/com/sali/anikfood/app/viewmodel/AddDataViewModel.kt
+++ app/src/main/java/com/sali/anikfood/app/viewmodel/AddDataViewModel.kt
@@ -1,9 +1,14 @@
 package com.sali.anikfood.app.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sali.anikfood.data.entity.Food
+import com.sali.anikfood.data.entity.User
 import com.sali.anikfood.domain.usecase.food.AddFood
 import com.sali.anikfood.domain.usecase.user.AddUser
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,4 +16,13 @@ class AddDataViewModel @Inject constructor(
     private val addUser: dagger.Lazy<AddUser>,
     private val addFood: dagger.Lazy<AddFood>
 ) : ViewModel() {
+
+    fun addUser(userName: String) = viewModelScope.launch(Dispatchers.IO) {
+        addUser.get().invoke(User(userId = 0, userName = userName))
+    }
+
+    fun addFood(foodName: String) = viewModelScope.launch(Dispatchers.IO) {
+        addFood.get().invoke(Food(foodId = 0, foodName = foodName))
+    }
+
 }
--- app/src/main/java/com/sali/anikfood/app/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/sali/anikfood/app/viewmodel/MainViewModel.kt
@@ -1,6 +1,10 @@
 package com.sali.anikfood.app.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sali.anikfood.data.entity.Favorite
+import com.sali.anikfood.data.entity.Food
+import com.sali.anikfood.data.entity.User
 import com.sali.anikfood.domain.usecase.favorite.AddFavorite
 import com.sali.anikfood.domain.usecase.favorite.DeleteFavorite
 import com.sali.anikfood.domain.usecase.favorite.GetUserFavorites
@@ -11,6 +15,10 @@ import com.sali.anikfood.domain.usecase.user.DeleteUser
 import com.sali.anikfood.domain.usecase.user.GetAllUsers
 import com.sali.anikfood.domain.usecase.user.GetUser
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,11 +26,58 @@ class MainViewModel @Inject constructor(
     private val getAllUsers: GetAllUsers,
     private val getUser: dagger.Lazy<GetUser>,
     private val getAllFoods: GetAllFoods,
-    private val deleteUser: dagger.Lazy<DeleteUser>,
-    private val deleteFood: dagger.Lazy<DeleteFood>,
     private val getUserFavorites: dagger.Lazy<GetUserFavorites>,
     private val getUserFavoriteFoods: dagger.Lazy<GetUserFavoriteFoods>,
+    private val deleteUser: dagger.Lazy<DeleteUser>,
+    private val deleteFood: dagger.Lazy<DeleteFood>,
     private val addFavorite: dagger.Lazy<AddFavorite>,
     private val deleteFavorite: dagger.Lazy<DeleteFavorite>,
 ) : ViewModel() {
+
+    private val _allUsers = MutableStateFlow<List<User>>(emptyList())
+    val allUsers get() = _allUsers.asStateFlow()
+    fun getAllUsers() = viewModelScope.launch(Dispatchers.IO) {
+        _allUsers.value = getAllUsers.invoke()
+    }
+
+    private val _user = MutableStateFlow<User?>(null)
+    val user get() = _user.asStateFlow()
+    fun getUser(userId: Int) = viewModelScope.launch(Dispatchers.IO) {
+        _user.value = getUser.get().invoke(userId)
+    }
+
+    private val _userFavorites = MutableStateFlow<List<Favorite>>(emptyList())
+    val userFavorites get() = _userFavorites.asStateFlow()
+    fun getUserFavorites(userId: Int) = viewModelScope.launch(Dispatchers.IO) {
+        _userFavorites.value = getUserFavorites.get().invoke(userId)
+    }
+
+    private val _userFavoriteFoods = MutableStateFlow<List<Food>>(emptyList())
+    val userFavoriteFoods get() = _userFavoriteFoods.asStateFlow()
+    fun getUserFavoriteFoods(foodIds: List<Int>) = viewModelScope.launch(Dispatchers.IO) {
+        _userFavoriteFoods.value = getUserFavoriteFoods.get().invoke(foodIds)
+    }
+
+    private val _allFoods = MutableStateFlow<List<Food>>(emptyList())
+    val allFoods get() = _allUsers.asStateFlow()
+    fun getAllFoods() = viewModelScope.launch(Dispatchers.IO) {
+        _allFoods.value = getAllFoods.invoke()
+    }
+
+    fun deleteUser(user: User) = viewModelScope.launch(Dispatchers.IO) {
+        deleteUser.get().invoke(user)
+    }
+
+    fun deleteFood(food: Food) = viewModelScope.launch(Dispatchers.IO) {
+        deleteFood.get().invoke(food)
+    }
+
+    fun addFavorite(favorite: Favorite) = viewModelScope.launch(Dispatchers.IO) {
+        addFavorite.get().invoke(favorite)
+    }
+
+    fun deleteFavorite(favorite: Favorite) = viewModelScope.launch(Dispatchers.IO) {
+        deleteFavorite.get().invoke(favorite)
+    }
+
 }
